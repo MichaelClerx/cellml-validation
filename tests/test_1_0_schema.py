caef958c1a390146d657eeafd354800cdb269838
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2019-01-18 10:24:07 +0000

    Rewrote schema 1.0 tests with fixtures.

--- tests/test_1_0_schema.py
+++ tests/test_1_0_schema.py
@@ -4,8 +4,8 @@
 from __future__ import absolute_import, division
 from __future__ import print_function, unicode_literals
 
-
 import os
+import pytest
 from lxml import etree
 
 from .shared import (
@@ -16,32 +16,51 @@ from .shared import (
 )
 
 
-def test_a_model():
-    # Create parser that can resolve URLs
-    p = etree.XMLParser(no_network=True)
-    p.resolvers.add(SchemaResolver())
+@pytest.fixture
+def schema_parser():
+    """ Returns a parser that can resolve schema locations. """
+    parser = etree.XMLParser(no_network=True)
+    parser.resolvers.add(SchemaResolver())
+    return parser
 
-    # Load schema
-    f = cellml('cellml_1_0.xsd')
-    assert os.path.isfile(f)
-    schema = etree.parse(f, p)
-    schema = etree.XMLSchema(schema)
 
+@pytest.fixture
+def schema(schema_parser):
+    """ Returns a parsed schema object. """
+    filename = cellml('cellml_1_0.xsd')
+    assert os.path.isfile(filename)
+    return etree.XMLSchema(etree.parse(filename, schema_parser))
+    return schema
+
+
+def assert_valid_with_schema(filename, schema, schema_parser):
+    """
+    Parses a model and a schema, asserting the model is valid against the
+    schema.
+    """
     # Parse CellML file
-    f = model('aslanidi_atrial_model_2009_LindbladCa_corrected.cellml')
+    filename = model(filename)
+    assert os.path.isfile(filename)
+    xml = etree.parse(filename, schema_parser)
+
+    # Check if namespace is set correctly (for a nicer error message)
+    tag = etree.QName(xml.getroot().tag)
+    assert tag.namespace == CELLML_1_0_NS
+
+    # Validate
+    schema.assertValid(xml)
+
+
+def test_a_model(schema, schema_parser):
+    assert_valid_with_schema(
+        'aslanidi_atrial_model_2009_LindbladCa_corrected.cellml',
+        schema,
+        schema_parser
+    )
     #f = cellml('beeler_reuter_1977.cellml')
     #f = cellml('espinosa_1998_hypertrophic.cellml')
     #f = cellml('vanderpol_vandermark_1928.cellml')
     #f = cellml('ohara_rudy_cipa_v1_2017.cellml')
     #f = cellml('noble_varghese_kohl_noble_1998_c.cellml')
     #f = cellml('test_simple_odes.cellml')
-    assert os.path.isfile(f)
-    x = etree.parse(f, p)
-
-    # Check if namespace set
-    tag = etree.QName(x.getroot().tag)
-    assert tag.namespace == CELLML_1_0_NS
-
-    # Validate
-    schema.assertValid(x)
 
