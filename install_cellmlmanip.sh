4c22eba816b21289e82a4b610ae4b8f4e1e23acd
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2019-07-12 13:43:29 +0100

    Fixed cellmlmanip import

--- install_cellmlmanip.sh
+++ install_cellmlmanip.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-pip install -e git://github.com/ModellingWebLab/cellmlmanip.git@feature/cellml-parsing#egg=cellmlmanip
-pip install -e git://github.com/ModellingWebLab/weblab_cg@fc-refactoring#egg=weblab_cg
+pip install -e git://github.com/ModellingWebLab/cellmlmanip.git#egg=cellmlmanip
+#pip install -e git://github.com/ModellingWebLab/weblab_cg@fc-refactoring#egg=weblab_cg
 
