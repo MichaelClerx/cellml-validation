65195d15001b935c4dc059012f568360d747c88c
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2020-02-25 21:44:46 +0000

    Removed cellmlmanip install script

--- install_cellmlmanip.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-pip install -e git://github.com/ModellingWebLab/cellmlmanip.git#egg=cellmlmanip
-#pip install -e git://github.com/ModellingWebLab/weblab_cg@fc-refactoring#egg=weblab_cg
-
